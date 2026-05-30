5a04bbeccf84c8a82d5a12cbb603b2d1eb9ffcc0
winnershock <winnersolarte749@gmail.com>
2026-05-29 20:50:48 -0500

    exo4_ex05

---
+++ ex04/ex04.js
@@ -0,0 +1,26 @@
+let nombre, edad, direccion, movil, email; // se puede declarar varias variables del mismo tipo.
+
+// prompt es una palabra reservada en JavaScript que despliega una funcion y sale un mensaje de tipo alert para el usuario.
+
+nombre = prompt("escriba su nombre: ")// pedir datos al ususario.
+document.write("Bienvenido: ", nombre, "<br>"); 
+console.log("Bienvenido: ", nombre);
+
+edad  = prompt("escriba su edad: ")// pedir datos al ususario.
+document.write("Tu edad es : ", edad,"<br>"); 
+console.log("Tu edad es: ", edad);
+
+direccion = prompt("escriba su direccion : ")// pedir datos al ususario.
+document.write("tu direccion es: ", direccion, "<br>"); 
+console.log("tu direccion es: ", direccion);
+
+movil = prompt("escriba su movil: ")// pedir datos al ususario.
+document.write("tu movil es: ", movil, "<br>"); 
+console.log("tu movil es : ", movil);
+
+email = prompt("escriba su email: ")// pedir datos al ususario.
+document.write("tu email es: ", email, "<br>"); 
+console.log("tu email es: ", email);
+
+
+
