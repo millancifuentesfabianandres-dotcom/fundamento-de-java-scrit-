807ff1629484673727b2608778bb72270d0408d3
winnershock <winnersolarte749@gmail.com>
2026-06-11 21:18:21 -0500

    funciones

---
+++ Funciones.py
@@ -0,0 +1,5 @@
+def suma(x, y):
+    return x + y
+
+resultado = suma(5,3)    
+print(resultado)# Imprime 8
---
+++ funciones.js
@@ -0,0 +1,14 @@
+function saludar () {
+    console.log("Bienvenidos aun saludo funcion")
+}
+
+saludar();
+
+// Set- ExecutionPolicy RemoteSigned
+// Dar permisos de ejecucion 
+
+function suma (x, y) {
+    return x + y;
+}
+let resultado = suma(5, 3); // La variable resultado esta invocando la funcion suma
+console.log(resultado); // Imprime 8
